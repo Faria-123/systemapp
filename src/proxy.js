76b982bfdc7785c76e6a3336d9709c4f5605be2e
Faria-123 <mrsnoushin1987@gmail.com>
2026-08-16 02:40:59 +0600

    here enternal

---
+++ src/proxy.js
@@ -0,0 +1,27 @@
+
+
+import { NextResponse } from "next/server";
+import { auth } from "./lib/auth";
+
+export const proxy = async (request) => {
+    const session = await auth.api.getSession({
+        headers: request.headers,
+    });
+
+    if (!session) {
+        return NextResponse.redirect(
+            new URL("/signin", request.url)
+        );
+    }
+
+    return NextResponse.next();
+};
+
+export const config = {
+    matcher: [
+        "/my-bookings",
+        "/manage-facilities/:path*",
+        "/allfacilities/:path/:path*",
+        "/addfacilities"
+    ],
+};
